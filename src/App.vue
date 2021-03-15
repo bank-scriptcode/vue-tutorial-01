39a898a39f3fd16688fabe9239d21b4e68f6daca
Teerapong Songsantivorakul <teerapong.songsan@gmail.com>
2021-03-15 13:51:39 +0700

    test-vue

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h1>{{ count }}</h1>
+    <button @click="onClickCount">Click</button>
+    <button @click="onClickClear">Clear</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "Test-VueJs",
+      count: 0,
+    };
+  },
+  methods: {
+    onClickCount() {
+      this.count = this.count + 1;
+    },
+    onClickClear() {
+      this.count = 0;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
